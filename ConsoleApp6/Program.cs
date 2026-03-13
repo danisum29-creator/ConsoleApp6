1f5806d1dc404f176e8c74744baab845fc93bb6c
Daniel S. <edvindaniel427@gmail.com>
2026-03-13 08:21:34 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class dispositivo
+        {
+            public string Nombre;
+            public double resultado1;
+            public double resultado2; 
+            public double resultado3;
+
+            public double CalcularPromedio()
+            {
+                return ((resultado1+ resultado2+resultado3)/3);
+            }
+        }
+    }
+}
